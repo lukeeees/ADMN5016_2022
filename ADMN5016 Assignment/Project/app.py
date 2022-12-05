85c09c88a13845aaedc5478783b66aac4d29d9b6
L <lukelaylo@gmail.com>
2022-12-05 16:05:38 -0500

    Update files

--- ADMN5016 Assignment/Project/app.py
+++ ADMN5016 Assignment/Project/app.py
@@ -3,8 +3,15 @@ from predict_page import show_predict_page
 from explore import show_explore_page
 #py -m streamlit run app.py
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+st.sidebar.camera_input("Discussing Now")
+
+
+
 
 if page == "Predict":
     show_predict_page()
 else:
     show_explore_page()
+
+st.markdown("---")
+st.write("Created by: Luke and Ziyang")
--- ADMN5016 Assignment/Project/predict_page.py
+++ ADMN5016 Assignment/Project/predict_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+import time
 
 
 def load_model():
@@ -15,41 +16,42 @@ model_loaded = data['model']
 def show_predict_page():
     st.title("CellPhone Price Predictor")
     st.write("""### We need some information to predict the price of Mobile Phone""")
-
+    col1, col2 ,col3= st.columns(3)
     criteria = {
-        "No": 0,
-        "Yes": 1
+        "Yes": 1,
+        "No": 0
     }
 
-    battery_power = st.slider("Battery Power", 500, 2000, 600)  #1
-    blue = st.selectbox("Bluetooth", criteria) #2
-    clock_speed = st.slider("Clock Speed", 0,3,1) #3
-    dual_sim = st.selectbox("Dual Sim", criteria) #4
-    fc = st.slider("Front Camera",0,19,2) #5
-    four_g = st.selectbox("4G", criteria) #6 
-    int_memory = st.slider("Internal Memory (Gigabytes)", 2,64,5) #7
-    m_dep = st.slider("Mobile Depth (cm)", 0.1,1.0,0.2) #8
-    mobile_wt = st.slider("Mobile Weight", 80,200,100) #9
-    n_cores = st.slider("Core Processor",1,8,2) #10
-    pc = st.slider("Back Camera",0,20,10) #11
-    px_height = st.slider("Pixel Resolution height",0,1960,100) #12 
-    px_width = st.slider("Pixel Resolution width",500,1998,100) #13
-    ram = st.slider("RAM",256,3998,1256)    #14
-    sc_height = st.slider("Screen height",5,19,12)  #15
-    sc_width = st.slider("Screen width",0,18,5) #16
-    talk_time = st.slider("Longest time that a single battery charge will last",2,20,11) #17
-    touch_screen = st.selectbox("Touch Screen", criteria) #18
-    wifi = st.selectbox("Wifi", criteria)
+    battery_power = col1.slider("Battery Power", 500, 2000, 600)  #1
+    blue = col1.selectbox("Bluetooth", criteria) #2
+    clock_speed = col1.slider("Clock Speed", 0,3,1) #3
+    dual_sim = col1.selectbox("Dual Sim", criteria) #4
+    fc = col1.slider("Front Camera",0,19,2) #5
+    four_g = col1.selectbox("4G", criteria) #6 
+    int_memory = col1.slider("Internal Memory (Gigabytes)", 2,64,5) #7
+    m_dep = col1.slider("Mobile Depth (cm)", 0.1,1.0,0.2) #8
+    mobile_wt = col1.slider("Mobile Weight", 80,200,100) #9
+    n_cores = col1.slider("Core Processor",1,8,2) #10
+    pc = col1.slider("Back Camera",0,20,10) #11
+    px_height = col1.slider("Pixel Resolution height",0,1960,1000) #12 
+    px_width = col1.slider("Pixel Resolution width",500,1998,1000) #13
+    ram = col2.slider("RAM",256,3998,1256)    #14
+    sc_height = col2.slider("Screen height",5,19,12)  #15
+    sc_width = col2.slider("Screen width",0,18,5) #16
+    talk_time = col2.slider("Longest time that a single battery charge will last",2,20,11) #17
+    touch_screen = col2.selectbox("Touch Screen", criteria) #18
+    wifi = col2.selectbox("Wifi", criteria)
 
-
-
-   # if ok:
     X = np.array([[battery_power,conv(blue), clock_speed,conv(dual_sim),fc,conv(four_g),int_memory,m_dep,mobile_wt,n_cores,pc,px_height,px_width,ram,sc_height,sc_width,talk_time,conv(touch_screen),conv(wifi)]])
-    #st.write(X)
     pred= model_loaded.predict(X)
-    message = "Prediction: The CellPhone is categorized as: "+messages(pred[0])
-    st.empty()
-    st.info(message,icon="✅")
+
+    progress_bar = col2.progress(0)
+    for perc_completed in range(100):
+        time.sleep(0.050)
+        progress_bar.progress(perc_completed+1)
+    
+    message = "Prediction: "+messages(pred[0])
+    col2.info(message,icon="✅")
 
 def conv(x):
     if x :
