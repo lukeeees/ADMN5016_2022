c149ca90343b45a04dead30338c6ffc58754414c
L <lukelaylo@gmail.com>
2022-12-07 09:09:47 -0500

    Update

---
+++ ADMN5016 Assignment/Project/explore.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+
+def show_explore_page():
+
+    df = pd.read_csv("MobilePrice.csv")
+    st.title("Explore the Dataset")
+    st.dataframe(df)
+    
+
+    
+    
+    
+
+
+
